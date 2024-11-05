1bab13b097c4c0cd80a75689cb7cc66e052be1e0
Xiang990293 <990293kwi@gmail.com>
2024-11-05 15:28:31 +0800

    ui complete 0.0.1

--- tamade/main.py
+++ tamade/main.py
@@ -1,8 +1,37 @@
 import flet as ft
 
-
 def main(page: ft.Page):
-    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
+    page.bgcolor = ft.colors.BLACK
+    
+        
+    def icon_pressed(e):
+        counter.value = str(int(counter.value)+1)
+        page.update()
+    
+    counter = ft.Text("0",scale=1.3)
+    page.add(
+        ft.Column(
+            controls=[
+                ft.Row(
+                    controls=[
+                        ft.TextButton(
+                            content=ft.Image(src="assets/TMD4_Original.jpg"),
+                            width=page.width.__float__() * 0.5,
+                            on_click=icon_pressed
+                        ),
+                    ],
+                    alignment=ft.MainAxisAlignment.CENTER
+                ),
+                ft.Row(
+                    controls=[
+                        counter
+                    ],
+                    alignment=ft.MainAxisAlignment.CENTER
+                )
+            ],
+            alignment=ft.MainAxisAlignment.CENTER
+        )
+    )
 
 
 ft.app(main)
