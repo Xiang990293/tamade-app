374c93ee9cc9e5f57ea3c86778610af26bdcc0a4
Xiang990293 <990293kwi@gmail.com>
2024-11-06 16:08:18 +0800

    tamade ui and function complete 0.0.1

--- tamade/main.py
+++ tamade/main.py
@@ -2,36 +2,44 @@ import flet as ft
 
 def main(page: ft.Page):
     page.bgcolor = ft.colors.BLACK
+    page.adaptive = True
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.MainAxisAlignment.CENTER
     
-        
     def icon_pressed(e):
         counter.value = str(int(counter.value)+1)
+        tamade.play()
         page.update()
     
-    counter = ft.Text("0",scale=1.3)
+    def icon_pressing(e):
+        counter.value = "0"
+        page.update()
+    
+    counter = ft.Text("0",scale=2)
+    tamade = ft.Audio(src="快吃你他媽的.wav",autoplay=False, volume=0.3)
+    tamaimg = ft.Image(src="TMD4_Original.jpg")
+
+    page.overlay.append(tamade)
     page.add(
         ft.Column(
-            controls=[
-                ft.Row(
-                    controls=[
-                        ft.TextButton(
-                            content=ft.Image(src="assets/TMD4_Original.jpg"),
-                            width=page.width.__float__() * 0.5,
-                            on_click=icon_pressed
-                        ),
-                    ],
-                    alignment=ft.MainAxisAlignment.CENTER
+            controls=[ 
+                ft.Container(
+                    content=tamaimg,
+                    on_click=icon_pressed,
+                    on_long_press=icon_pressing,
+                    alignment=ft.alignment.center,
+                    width=page.width.__float__() * 0.9,
+                    height=page.height.__float__() * 0.4
                 ),
-                ft.Row(
-                    controls=[
-                        counter
-                    ],
-                    alignment=ft.MainAxisAlignment.CENTER
-                )
+                counter
             ],
-            alignment=ft.MainAxisAlignment.CENTER
+            alignment=ft.alignment.center,
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER
         )
     )
 
+ft.app(
+    main,
+    assets_dir="assets",
 
-ft.app(main)
+)
